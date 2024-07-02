ca65fb52a7bf1f46c31746137160262bd559fc18
Luca Raveri <lucaraveri993@gmail.com>
2024-07-02 16:18:36 +0200

    feat(style): mobile menu

---
+++ src/components/navbar/MobileMenuToggle.tsx
@@ -0,0 +1,41 @@
+'use client';
+import { useState } from 'react';
+import NavButton from '@/components/navbar/NavButton';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons/faTimes';
+import { faBars } from '@fortawesome/free-solid-svg-icons/faBars';
+
+const MobileMenuToggle = () => {
+	const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+	const toggleMobileMenu = () => {
+		setIsMobileMenuOpen(!isMobileMenuOpen);
+	};
+
+	return (
+		<>
+			<button onClick={toggleMobileMenu} className="text-teal focus:outline-none mr-6 mt-2 text-2xl">
+				<FontAwesomeIcon icon={isMobileMenuOpen ? faTimes : faBars} />
+			</button>
+			{isMobileMenuOpen && (
+				<div className="absolute top-full left-0 w-full bg-white">
+					<div className="flex flex-col items-center space-y-4 mt-4">
+						<NavButton href="/blog" label="Blog" />
+						<NavButton href="/projects" label="Projects" />
+						<NavButton href="/experience" label="Experience" />
+						<NavButton href="/about" label="About" />
+						{/*<a*/}
+						{/*	href="/luca_raveri_cv.pdf"*/}
+						{/*	download*/}
+						{/*	className="bg-teal text-white py-2 px-4 rounded-full border-2 border-teal hover:bg-lightgray hover:text-teal"*/}
+						{/*>*/}
+						{/*	Download CV*/}
+						{/*</a>*/}
+					</div>
+				</div>
+			)}
+		</>
+	);
+};
+
+export default MobileMenuToggle;
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,21 +1,28 @@
+'use server';
 import Link from 'next/link';
 import NavButton from '@/components/navbar/NavButton';
 import Image from 'next/image';
+import MobileMenuToggle from '@/components/navbar/MobileMenuToggle';
 
-export default function Navbar() {
+export default async function Navbar() {
 	return (
-		<nav className="w-full bg-white text-white py-4 border-b border-lightgray">
+		<nav className="w-full bg-white text-white py-4 relative">
 			<div className="container mx-auto flex justify-between items-center">
 				<div className="flex items-center">
 					<Link href={'/'}>
-						<Image src="/profile.png" alt="Logo" width={40} height={40}
-							   className="w-10 h-10 rounded-full bg-white border-2 border-aqua mr-4" />
+						<Image
+							src="/profile.png"
+							alt="Logo"
+							width={40}
+							height={40}
+							className="w-10 h-10 rounded-full bg-white border-2 border-aqua mr-4 ml-4 sm:ml-0"
+						/>
 					</Link>
 					<Link href={'/'}>
 						<h1 className="text-teal text-xl font-extrabold">Luca Raveri</h1>
 					</Link>
 				</div>
-				<div className="flex-grow flex justify-center">
+				<div className="hidden md:flex flex-grow justify-center">
 					<div className="flex items-center space-x-4">
 						<NavButton href="/blog" label="Blog" />
 						<NavButton href="/projects" label="Projects" />
@@ -23,13 +30,17 @@ export default function Navbar() {
 						<NavButton href="/about" label="About" />
 					</div>
 				</div>
-				<div>
+				<div className="hidden md:block">
 					<a
 						href="/luca_raveri_cv.pdf"
 						download
-						className="bg-teal text-white py-2 px-4 rounded-full border-2 border-teal hover:bg-lightgray hover:text-teal">
+						className="bg-teal text-white py-2 px-4 rounded-full border-2 border-teal hover:bg-lightgray hover:text-teal"
+					>
 						Download CV
 					</a>
+				</div>
+				<div className="md:hidden">
+					<MobileMenuToggle />
 				</div>
 			</div>
 		</nav>
