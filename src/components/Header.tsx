1b92fe453bbf59487fb9638ac017c9d8b379bae4
Luca Raveri <lucaraveri993@gmail.com>
2024-07-03 12:38:19 +0200

    feat(style): update style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,6 +4,6 @@ interface HeaderProps {
 
 export default function Header({ title }: HeaderProps) {
 	return (
-		<h1 className="text-3xl font-bold mb-6 text-center sm:py-4 underline underline-color-teal">{title}</h1>
+		<h1 className="text-3xl font-bold mb-6 text-center py-4 underline underline-color-teal">{title}</h1>
 	);
 }
