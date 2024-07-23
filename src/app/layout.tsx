f4318f5f17ead4de0e2131bb678bd2d5f3df342b
Luca Raveri <lucaraveri993@gmail.com>
2024-07-23 18:08:35 +0200

    feat(chatbot): add chatbot script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Atkinson_Hyperlegible } from 'next/font/google';
 import './globals.css';
 import Navbar from '@/components/navbar/Navbar';
 import Footer from '@/components/footer/Footer';
+import Script from 'next/script';
 
 export const metadata: Metadata = {
 	title: 'Luca Raveri',
@@ -28,6 +29,28 @@ export default function RootLayout({
 			<main className="flex-grow">{children}</main>
 			<Footer></Footer>
 		</div>
+		<script
+			dangerouslySetInnerHTML={{
+				__html: `
+              (function() {
+                if (window.innerWidth > 1024) {
+                  var script = document.createElement('script');
+                  script.src = "https://cdn.jsdelivr.net/gh/lraveri/gpt-delivr@v0.1.1/packages/widget/dist/bundle.js";
+                  script.onload = function() {
+                    initializeChat({
+                      baseURL: "https://monorepo-lerna-khaki.vercel.app",
+                      baseColor: "#255957",
+                      assistantId: "asst_7A5Fy0UskQKTVtOWFLOGwd3B",
+                      initialMessage: "Ciao, sono l'assistente di Luca Raveri, come posso aiutarti?",
+                      module: "luca",
+                    });
+                  };
+                  document.head.appendChild(script);
+                }
+              })();
+            `
+			}}
+		/>
 		</body>
 		</html>
 	);
